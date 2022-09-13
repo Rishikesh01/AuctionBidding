b463c8a1f737e63bc7c6ba595e2914ed42bcf3ff
rishikesh <rsbetigeri@gmail.com>
2022-09-13 08:41:38 +0530

    BiddingClient

---
+++ BiddingClient/src/main/java/com/client/biddingclient/BiddingClientApplication.java
@@ -0,0 +1,13 @@
+package com.client.biddingclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiddingClientApplication.class, args);
+    }
+
+}
